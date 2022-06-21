6ffeca95b82e8d62db29b6ff74aa59bf25ba356c
HANSIeltsKing <96668549+HANSIeltsKing@users.noreply.github.com>
2022-06-21 20:49:04 +0100

    Update Triangular Route Generation.py

--- Triangular Route Generation.py
+++ Triangular Route Generation.py
@@ -1,4 +1,10 @@
 
+# i don't like matrix expression because it is very complicated to show long routes, instead, numbered blocks are more convenient
+# eg. 4dim: ([1 2 3 4],[5 6 7 8],[9 10 11 12],[13 14 15 16])
+# if you want to generate the number of routes only just search LeetCode 63. Unique Paths II
+# it is very stupid to calculate 10dim because it has more than 5000 possible routes (cannot express) so i used 6dim instead
+# i will show you a more generalised answer (theoretically available for all dims) to express routes more quickly
+
 def triangle(n):
     l = []
     for i in range(n):
